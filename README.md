eaff207668813f53b5872a01a8a75f1174b9bb5a
David Liang <david.liang@outlook.com>
2019-02-14 14:25:53 -0800

    Use a self-signed certificate instead of the temporary key generated by AddDeveloperSigningCredential().

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # DL.STS
 A security token service built using ASP.NET Core and IdentityServer4. It uses ASP.NET Core Identity as user store, and Entity Framework Core as configuration and operational store.
+
+### Add Signing Credential
+For local development, you can use the built-in `.AddDeveloperSigningCredential()` to create temporary key material at startup time. But for production, we need to pass in either a self-signed certificate or a certificate from the certificate store.
+
+We might as well generate a self-signed one now.
+
+If you're running Windows 10, you can run this command on your administrative power shell:
+
+    New-SelfSignedCertificate 
+        -Type Custom 
+        -Subject "CN=XXXX" 
+        -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.3") 
+        -KeyUsage DigitalSignature 
+        -KeyAlgorithm RSA 
+        -KeyLength 2048 
+        -CertStoreLocation "Cert:\LocalMachine\My" 
+
+Replace XXXX with whatever command name you want.
+
+Then copy the thumb print from the self-signed certificate and replace the one in `appsettings.json`:
+
+    {
+        ...,
+        "IdentityServer": {
+            "ThumbPrint": "XXXX"
+        }
+    }
--- src/DL.STS.Host/Startup.cs
+++ src/DL.STS.Host/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace DL.STS.Host
 {
@@ -30,11 +32,15 @@ namespace DL.STS.Host
                 .AddMvc()
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
+            // Read thumbpaint from appsettings
+            string thumbPrint = _configuration.GetValue<string>("IdentityServer:ThumbPrint");
+
             services
                 .AddIdentityServer()
                 .AddEFConfigurationStore(dbConnectionString)
                 .AddEFOperationalStore(dbConnectionString)
-                .AddAspNetIdentity<AppUser>();
+                .AddAspNetIdentity<AppUser>()
+                .AddSigningCredential(LoadCertificate(thumbPrint));
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -55,12 +61,29 @@ namespace DL.STS.Host
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
+            app.UseIdentityServer();
+
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=dashboard}/{action=index}/{id?}");
             });
+        }
+
+        private X509Certificate2 LoadCertificate(string thumbPrint)
+        {
+            using (var store = new X509Store(StoreName.My, StoreLocation.LocalMachine))
+            {
+                store.Open(OpenFlags.ReadOnly);
+                var certCollection = store.Certificates.Find(X509FindType.FindByThumbprint, thumbPrint, validOnly: false);
+                if (certCollection.Count == 0)
+                {
+                    throw new Exception("No certificate found containing the specified thumbprint.");
+                }
+
+                return certCollection[0];
+            }
         }
     }
 }
